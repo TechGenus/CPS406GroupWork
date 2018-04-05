d894050b57fc644c282b02ae64a602b95e1223c6
luigibird <33492051+luigibird@users.noreply.github.com>
2018-04-05 15:12:09 -0400

    Add files via upload

--- MembershipManagementMachine/src/pc_user_interface/Main.java
+++ MembershipManagementMachine/src/pc_user_interface/Main.java
@@ -1,10 +1,24 @@
 package pc_user_interface;
 
+import java.io.File;
+import java.util.*;
+
 public class Main {
 	
 	public static void main(String[] args) {
-		//TreasurerUI treasurerUI = new TreasurerUI("Membership Management Machine", "Jedi Master Luigibird");
-		//CustomerUI customerUI = new CustomerUI("Welcome to LA Fitness");
-		CoachUI coachUI = new CoachUI();
+		LinkedList Users = new LinkedList();
+		IOWork decryptor = new IOWork();
+		File folder = new File("/users/");
+		File[] listOfFiles = folder.listFiles();
+
+		for (File file : listOfFiles) {
+		    if (file.isFile()) {
+		        System.out.println(file.getName());
+		        Users.add(decryptor.deserialize(file.getName()));
+		    }
+		}
+		
+		TreasurerUI treasurerUI = new TreasurerUI("Membership Management Machine", "Jedi Master Luigibird");
+		CustomerUI customerUI = new CustomerUI("Welcome to LA Fitness");
 	}
 }
